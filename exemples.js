6789d2af9ca2aa614bb72fd708abdfe4cdd4342a
José André <eujoseandre@gmail.com>
2022-01-25 14:30:34 -0300

    first commit
    lessons

---
+++ exemples.js
@@ -0,0 +1,23 @@
+const parametro01 = 'alura';
+const parametro02 = 'semana';
+
+outraFunction = (arrayParametros, parametro01, parametro02) => {
+    console.log(`${arrayParametros[0]}${parametro01}${arrayParametros[1]}${parametro02}`);
+}
+
+// As duas chamadas são equivalentes!
+outraFunction`Eu estudo na ${parametro01} todos os dias da ${parametro02}`;
+
+outraFunction(['Eu estudo na ', ' todos os dias da '], parametro01, parametro02);
+
+function negrito(arrayStrings, ...valores) {
+
+    return arrayStrings.reduce((acumulador, string, i) => {
+        return `${acumulador}${string}${valores[i] ? `<strong>${valores[i]}</strong>` : ''}`;
+    }, '');
+}
+
+const nome = 'José André';
+const redeSocial = '@eujoseandre';
+
+console.log(negrito`${nome} estuda desenvolvimento Front-End na Alura e você pode encontrá-lo nas redes sociais procurando por ${redeSocial}`);
